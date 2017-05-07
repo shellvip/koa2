fec25d833f623f8dd2c0a07e0b6b4d71056869d8
shellvip <44074055@qq.com>
2017-05-08 00:38:10 +0800

    request me

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-require('./src').start()
+const schedule = require('node-schedule');
+const rp = require('request-promise');
+const requestSelf = async ()=>{
+    console.log('request self at: ' + new Date());
+    await rp('https://appsc.herokuapp.com');
+};
+
+schedule.scheduleJob('30 */5 * * * *', () => {
+    requestSelf();
+});
+
+require('./src').start();
+
